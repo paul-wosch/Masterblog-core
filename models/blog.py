90ca0eb497fc4c9d39edf222eea433001ea7243f
Jan-Paul Wass <paul@wass-media.com>
2025-10-29 15:57:27 +0100

    feat(models): Add blog and post classes
    - Created Blog and Post classes to establish a foundation for a
      blogging application.
    - Post instances stored author, title, content, and likes, and
      supported updates and like increments.
    - Blog managed a collection of posts with methods to add, get, and
      delete entries.
    - Exposed models in the package for easier imports.
    - Provided a structured base for future CRUD features and improved
      maintainability by organizing code into modules.

---
+++ models/blog.py
@@ -0,0 +1,62 @@
+"""Provide the Blog class."""
+from post import Post
+
+
+class Blog:
+    """Manage a collection of blog posts."""
+
+    def __init__(self, blog_posts: list):
+        """Initialize a Blog instance."""
+        self.posts = [Post(**item) for item in blog_posts]
+
+    def get(self, id):
+        """Return a single post object by its id or None if no matching was found."""
+        return next((post for post in self.posts if post.id == id), None)
+
+    def get_posts(self):
+        """Return all posts as a list of dictionaries."""
+        return [post.get() for post in self.posts]
+
+    def add(self, post: dict | Post = None, **kwargs):
+        """Add a post to the collection.
+
+        Input:
+            - post object instance
+            - post as dictionary
+            - post as key-word arguments
+        """
+        if isinstance(post, Post):
+            new_post = post
+        elif isinstance(post, dict):
+            new_post = Post(**post)
+        else:
+            new_post = Post(**kwargs)
+        self.posts.append(new_post)
+
+    def delete(self, post):
+        """Delete the given post from the collection."""
+        self.posts.remove(post)
+
+
+def main():
+    """Main function for testing."""
+    """
+    blog_posts = [{"id": 1, "author": "John Doe", "title": "First Post", "content": "This is my first post.", "likes": 0},
+                  {"id": 2, "author": "Jane Doe", "title": "Second Post", "content": "This is another post.", "likes": 0},
+                  {"id": 3, "author": "Anonymous", "title": "Third Post", "content": "This is another post.", "likes": 0},
+                  {"id": 4, "author": "Mad Max", "title": "Forth Post", "content": "This is another post.", "likes": 0},
+                  {"id": 5, "author": "Lisa Max", "title": "Fifth Post", "content": "This is another post.", "likes": 0},
+
+    ]
+    my_blog = Blog(blog_posts)
+    print(my_blog.get(1).content)
+    print(my_blog.get_posts())
+    my_blog.get(1).update(author="Test")
+    print(my_blog.get_posts())
+    my_blog.delete(my_blog.get(1))
+    print(my_blog.get_posts())
+    """
+
+
+if __name__ == "__main__":
+    main()
---
+++ models/post.py
@@ -0,0 +1,38 @@
+"""Provide the Post class."""
+
+
+class Post:
+    """Represent a specific post in a blog."""
+
+    def __init__(self, id: int, author: str, title: str, content: str, likes: int):
+        """Create a new Post instance."""
+        self.id = id
+        self.author = author
+        self.title = title
+        self.content = content
+        self.likes = likes
+
+    def get(self):
+        """Return a Post instance as dictionary."""
+        return self.__dict__
+
+    def update(self, author: str = None, title: str = None, content: str = None):
+        """Change author, title or content of a Post instance."""
+        if author:
+            self.author = author
+        if title:
+            self.title = title
+        if content:
+            self.content = content
+
+    def like(self):
+        """Increment the like value by one."""
+        self.likes += 1
+
+
+def main():
+    """Main function for testing."""
+
+
+if __name__ == "__main__":
+    main()
